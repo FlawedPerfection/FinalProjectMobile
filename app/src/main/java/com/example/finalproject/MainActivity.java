20abd24f57679ee32413f1015b54a13ae6e445f5
TuryAljaqobi <alja0058@algonquinlive.com>
2021-11-28 14:21:31 -0600

    WIP

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.finalproject;
 
 import static android.media.CamcorderProfile.get;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.util.Xml;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -19,11 +33,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ListView listViewNews;
     //Making our arraylist to store our News Items from the RSS feed
-
-
+    ArrayList<String> titles;
+    ArrayList<String> links;
 
     Toolbar toolbar;
-    private URL CbcNews;
+
+    private URL url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +46,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         toolbar = findViewById(R.id.toolbar);
 
+        listViewNews = findViewById(R.id.listViewNews);
+        titles = new ArrayList<>();
+        links = new ArrayList<>();
+
+        listViewNews.setOnItemClickListener((adapterView, view, position, l) -> {
+            Uri uri = Uri.parse(links.get(position));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        });
 
 
 
-               }
+        new ProcessInBackground().execute();
+    }
 
+
+    public InputStream getInputStream(URL url) {
+        try {
+            return url.openConnection().getInputStream();
+        } catch (IOException e) {
+            return null;
+        }
 
 
     }
+
+    public class ProcessInBackground extends AsyncTask<Integer, Void, Exception> {
+
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        Exception exception = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog.setMessage("Loading rss feed");
+            progressDialog.show();
+        }
+
+        @Override
+        protected Exception doInBackground(Integer... params) {
+            try {
+                URL url = new URL("http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml");
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(getInputStream(url), "UTF_8");
+
+                boolean insideItem = false;
+                int eventType = xpp.getEventType();
+
+                while (eventType != XmlPullParser.END_DOCUMENT)
+                {if (eventType == XmlPullParser.START_TAG) {
+                    if (xpp.getName().equalsIgnoreCase("item")){
+                        insideItem = true;
+                    }
+                    else if (xpp.getName().equalsIgnoreCase("title")){
+
+                        if (insideItem) {
+                            titles.add(xpp.nextText());
+                        }
+                    } else if (xpp.getName().equalsIgnoreCase("link")){
+                        if (insideItem){
+                            links.add(xpp.nextText());
+                        }
+                    }
+                }
+                else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")){
+                    insideItem=false;
+                }
+                    eventType=xpp.next();
+                }
+
+            } catch (MalformedURLException e) {
+
+                exception = e;
+            } catch (XmlPullParserException e) {
+                exception = e;
+            } catch (IOException e) {
+                exception = e;
+            }
+            return exception;
+        }
+
+        @Override
+        protected void onPostExecute(Exception s) {
+            super.onPostExecute(s);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, titles);
+            listViewNews.setAdapter(adapter);
+
+            progressDialog.dismiss();
+        }
+    }
+
+
+}
+
+
+
+
